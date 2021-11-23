029f87692958f466a374409c73e592477cfd6253
Nazar Dzhuruk <nazar.dzhuruk@gmail.com>
2021-11-23 19:28:22 +0100

    Add assignment methods

--- src/main/java/com/foxminded/dao/CourseDao.java
+++ src/main/java/com/foxminded/dao/CourseDao.java
@@ -3,4 +3,6 @@ package com.foxminded.dao;
 import com.foxminded.model.course.Course;
 
 public interface CourseDao extends SimpleDao<Course> {
+    void addStudents(int studentId, int courseId);
+    void addLectures(int lectureId, int courseId);
 }
--- src/main/java/com/foxminded/dao/TeacherDao.java
+++ src/main/java/com/foxminded/dao/TeacherDao.java
@@ -3,5 +3,6 @@ package com.foxminded.dao;
 import com.foxminded.model.teacher.Teacher;
 
 public interface TeacherDao extends SimpleDao<Teacher> {
+    void addLecture(int teacherId, int lectureId);
 
 }
