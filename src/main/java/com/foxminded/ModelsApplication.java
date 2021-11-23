5f1497f85f1f108ae0c97bcb3d3a1fbdb47364b5
Nazar Dzhuruk <nazar.dzhuruk@gmail.com>
2021-11-23 19:34:35 +0100

    Add service methods (WIP:)

--- src/main/java/com/foxminded/ModelsApplication.java
+++ src/main/java/com/foxminded/ModelsApplication.java
@@ -1,36 +1,59 @@
 package com.foxminded;
 
 import com.foxminded.dao.*;
+import com.foxminded.model.course.Course;
+import com.foxminded.model.course.CourseBuilderImpl;
+import com.foxminded.model.lecture.Lecture;
+import com.foxminded.model.lecture.LectureBuilderImpl;
+import com.foxminded.model.student.Student;
 import com.foxminded.model.student.StudentBuilder;
 import com.foxminded.model.student.StudentBuilderImpl;
-import com.foxminded.service.UniversityService;
+import com.foxminded.model.teacher.Teacher;
+import com.foxminded.model.teacher.TeacherBuilderImpl;
+import com.foxminded.service.logic.CourseService;
+import com.foxminded.service.logic.StudentService;
+import com.foxminded.service.logic.TeacherService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class ModelsApplication {
+
 
     private static StudentDao studentDao;
     private static TeacherDao teacherDao;
     private static CourseDao courseDao;
     private static LectureDao lectureDao;
-    private static UniversityService university;
+    private static TeacherService teacherService;
+    private static StudentService studentService;
+    private static CourseService courseService;
 
 
-    public ModelsApplication(StudentDao studentDao, UniversityService universityService, TeacherDao teacherDao, CourseDao courseDao, LectureDao lectureDao) {
+    public ModelsApplication(StudentService studentService, TeacherService teacherService,
+                             CourseService courseService, LectureDao lectureDao, CourseDao courseDao) {
         this.studentDao = studentDao;
         this.teacherDao = teacherDao;
         this.courseDao = courseDao;
         this.lectureDao = lectureDao;
-        this.university = universityService;
+        this.teacherService = teacherService;
+        this.studentService = studentService;
+        this.courseService = courseService;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, SQLException {
         SpringApplication.run(ModelsApplication.class, args);
 
         StudentBuilder studentBuilder = new StudentBuilderImpl();
+        Teacher teacher = new TeacherBuilderImpl().setId(2134).setName("Test").setSurname("Teast").build();
+        Lecture lecture = new LectureBuilderImpl().setId(123).setLectureName("Test").build();
+        Student student = new StudentBuilderImpl().setId(3221).setName("Test2").setSurname("Test2").build();
+        Course course = new CourseBuilderImpl().setCourseID(222).setCourseName("test").build();
 
-        university.removeStudent(12211);
+
+        courseService.addStudentToCourse(3221, 222);
 
 
 
