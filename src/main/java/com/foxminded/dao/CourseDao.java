4b12952f9cf92be783e89e73576971dc5338600b
Nazar Dzhuruk <nazar.dzhuruk@gmail.com>
2021-12-01 11:03:25 +0100

    Delete useless code

--- src/main/java/com/foxminded/dao/CourseDao.java
+++ src/main/java/com/foxminded/dao/CourseDao.java
@@ -3,6 +3,4 @@ package com.foxminded.dao;
 import com.foxminded.model.course.Course;
 
 public interface CourseDao extends SimpleDao<Course> {
-    void addStudents(int studentId, int courseId);
-    void addLectures(int lectureId, int courseId);
 }
